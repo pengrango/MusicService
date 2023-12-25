2ec14aafe1d88706e44a85617bde63689c827fe8
pengrango <penrod_zhao@hotmail.com>
2023-12-25 06:20:28 +0100

    return 502 for 5xx from MusicBrainz

--- src/main/java/com/musify/app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/musify/app/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.musify.app.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,9 +18,13 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         if (ex.getStatusCode().is4xxClientError()) {
             logger.error("Client error", ex);
             return ResponseEntity.status(ex.getStatusCode()).body("Error in Client request.");
+        } else if (ex.getStatusCode().is5xxServerError()) {
+            logger.error("The dependency service returns an error", ex);
+            return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body("We're currently experiencing issues with external services, impacting data retrieval.");
         } else {
-            logger.error("Internal error", ex);
-            return ResponseEntity.status(ex.getStatusCode()).body("Error in external service.");
+            logger.error("Unknown error", ex);
+            return ResponseEntity.status(ex.getStatusCode()).body("There are communication issue between us and external services. " +
+                "We are looking into it. Thank you for the patience.");
         }
     }
 
